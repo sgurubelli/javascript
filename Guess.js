51e53ab0a131b03abae249ee39163bffbeb4c5ce
sgurubelli <bsuryaguru@gmail.com>
2021-02-21 13:23:44 -0800

    Update Guess.js
    added loop

--- Guess.js
+++ Guess.js
@@ -1,11 +1,11 @@
 var number = Math.round(Math.random() * 100 + 1) ;
-var guess = prompt ("Guess a number between 1 and 100: ") ;
-
+var guess =  "";
+while (guess != number) {
+guess = prompt ("Guess a number between 1 and 100: ") ;
 if (guess > number)
-    console.log ("you are to high");
+    alert ("you are too high");
 else if (guess < number)
-    console.log ("you are to low");
-else {
-  console.log ("Correct you win!");
-
+    alert ("you are too low");
+else 
+  alert ("Correct you win!");
 }
